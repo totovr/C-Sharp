bfebce530c3f917f92a295319b2ee474cc6690b8
Antonio Vega <a.vegaramirez76@gmail.com>
2018-10-18 13:01:58 +0900

    Valorations

---
+++ Valorations/Valorations/BookValue.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Valorations
+{
+    class BookValue
+    {
+        List<float> valorations;
+
+        public BookValue()
+        {
+            valorations = new List<float>();
+        }
+            
+        public void AddValoration(float valoration)
+        {
+            valorations.Add(valoration);
+        }
+       
+    }
+}
---
+++ Valorations/Valorations/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Valorations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a new constructor
+            BookValue book = new BookValue();
+            book.AddValoration(3.5f);
+            book.AddValoration(5);
+        }
+    }
+}
