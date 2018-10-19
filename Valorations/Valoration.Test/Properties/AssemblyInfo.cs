ca7d5a60cc87bbc7b38b184ccca69e7e73c52022
Antonio Vega <a.vegaramirez76@gmail.com>
2018-10-19 15:11:18 +0900

    update

---
+++ Valorations/Valoration.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Valoration.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("UNITCOM PC")]
+[assembly: AssemblyProduct("Valoration.Test")]
+[assembly: AssemblyCopyright("Copyright © UNITCOM PC 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f9ebc14e-0c8f-418f-92f1-fc69350018b7")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
