0cb2aacb5c844c4bb32b83a66fd3cd448659efe8
Antonio Vega <a.vegaramirez76@gmail.com>
2018-10-22 18:14:48 +0900

    comment speech package

--- Valorations/Valorations/Program.cs
+++ Valorations/Valorations/Program.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Speech.Synthesis;
+// using System.Speech.Synthesis;
 using System.Text;
 using System.Threading.Tasks;
 
