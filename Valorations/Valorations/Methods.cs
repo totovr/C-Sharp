f2dbe4d8bce624a4729d2a8f3b4d631eca4e3d03
Antonio Vega <a.vegaramirez76@gmail.com>
2018-10-23 14:57:32 +0900

    method
    structure of a class method

---
+++ Valorations/Valorations/Methods.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Valorations
+{
+    class Methods
+    {
+        // if I know that is not going to return nothinh
+        // Just in this assambly
+        internal void MethodExample(int i, string s, float f)
+        {
+            value = i;
+        }
+
+        // Is possible to access from other assambly
+        public void MethodExample(int i, float f)
+        {
+            value = i;
+        }
+
+        // Just is able to access in this class
+        private void MethodExample()
+        {
+            value = 5;
+        }
+        // This is the scope
+        int value = 0;
+    }
+}
