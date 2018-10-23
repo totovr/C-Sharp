ab1e630add4a4f5862612e1d6f24def8b4b17a0d
Antonio Vega <a.vegaramirez76@gmail.com>
2018-10-23 17:07:08 +0900

    States

---
+++ Valorations/Valorations/ControlStructures/States.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Valorations.ControlStructures
+{
+    class States
+    {
+        /*
+         * 
+         // State of declaration
+         int count;
+         
+        // State of assignation
+        count = 1; 
+
+        // State of declaration with assignation
+        int[] arrayInts = {15, 32, 108, 74, 9};
+
+        // Multiple block of code
+        foreach (int value in arrayInts)
+        {
+            // Do something
+        }
+
+        // Summon state
+        Hello();
+
+        // Expresion state
+        counter ++;
+
+         */
+
+        /// Types of most used states
+        /// - Declaration
+        /// - Expresion (counter++)
+        /// Selection (if then else, switch case)
+        /// Iteration or bucle
+        /// jump
+        /// Exeption
+    }
+}
