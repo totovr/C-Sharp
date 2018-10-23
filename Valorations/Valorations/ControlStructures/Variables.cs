a77d14102a7c3653d9e94a651ae4f30566954f65
Antonio Vega <a.vegaramirez76@gmail.com>
2018-10-23 17:26:31 +0900

    Operations and expresions

---
+++ Valorations/Valorations/ControlStructures/Variables.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Valorations.ControlStructures
+{
+    class Variables
+    {
+        // Value type
+        /*
+        double myDouble = 0,123456789;
+        enum {none, north, south, south, east, west}
+        float myFloat = 0.12345f;
+        int 555;
+
+        // Char save characters 
+        Char myChar = "Hello world";
+        myChar.
+
+        bool mybBool = true;
+        bool myBool = false;
+
+        // Less commons but more complex to manage puntual data
+        long
+        sbyte
+        short
+        struct
+        uint
+        ulong
+        ushort
+        */
+
+        // Reference type
+        /*
+        class (MyClass myClassVariable = new MyClass())
+        string greetings = "Hello world";
+
+        delegate
+        dynamic
+        interface
+        object
+
+        Interpolated Strings
+
+         */
+    }
+}
