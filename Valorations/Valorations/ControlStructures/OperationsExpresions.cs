cbad36e563c8549e80cffe5e0022af36b4298aad
Antonio Vega <a.vegaramirez76@gmail.com>
2018-10-23 18:00:01 +0900

    type of operator and expressions

--- Valorations/Valorations/ControlStructures/OperationsExpresions.cs
+++ Valorations/Valorations/ControlStructures/OperationsExpresions.cs
@@ -8,5 +8,98 @@ namespace Valorations.ControlStructures
 {
     class OperationsExpresions
     {
+        int x = 10;
+
+        // This is an expresion
+        // if ((x != 10) && (x > 5) || ((x > 20) && (x < 25)))
+
+        /*
+         * There are two types of expresion
+         * 
+         * Literal
+         * int i = 5;
+         * 
+         * Name 
+         * int numMax = 5;
+         * 
+         * Expression summon
+         * ();
+         */
+
+        // Operators
+        /*
+        
+        Primary operators
+
+        x.y access to the members of the class
+        x?.y access to the members with a conditional
+        M(x) Summon of the method (with parameters that is optiona)
+        a[x] Possition of the array
+        a?[x] Position of the array with conditional 
+        x++ Post increment
+        x-- Post decrement
+        new T(...) Create an object
+        new T(...)(...) Create an object with initializator
+
+        Unity operators
+
+        +x Indication positive number
+        -x Indicate negative number
+        ++ Pre increment
+        \\ Pre decrement
+        (T)x Conversion of explicit type
+
+        Operators of multiplication
+
+        * Multiplication
+        / Division
+        % Module
+
+        Adition operators
+
+        x + y Aditions, chain concatenations
+        x - y Substraction
+
+        Relation operators
+
+        x < y less than
+        x > y higher than 
+        x <= less or equal than
+        x >= higher or equal than
+
+        Equality operators
+
+        x == y Equal
+        x != y Not equal
+
+        Logical operators, Conditional
+
+        x && y Logic AND if (x == 10 && y == 20 && n == 40 && z == 60)
+                         {
+                            // If all the cases are true
+                         }
+        
+        x || y Conditional OR if (x == 10 || y == 20)
+                              {
+                                // If one of the cases is true
+                              }
+        
+         // Assignation
+
+        = Assignation
+        x op = y
+        +=, -=, *=, /=, %=, &=, |=, != (Compound assigners)
+
+        Association Operators 
+
+        int a, b, c;
+        c = 1;
+        a = b = c;
+
+        Adding parenthesis
+
+        int a = (4 * 5) + 20;
+
+         */
     }
 }
